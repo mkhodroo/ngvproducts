09f8924ebfdac3b17c83d231f7d755cd0831316d
mkhodroo <mkhodroo@yahoo.com>
2022-10-08 20:25:07 +0330

    home page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.dashboard.main');
+    return view('store.home.home');
 });
 
 Route::get('/dashboard', function () {
