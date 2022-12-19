11ccd83857460fd8d5863e8a4cd69b3ea62e3012
mohammad shahab <mkhodroo@yahoo.com>
2022-12-19 20:43:40 +0330

    Update single.blade.php

--- resources/views/store/products/single.blade.php
+++ resources/views/store/products/single.blade.php
@@ -1,28 +1,31 @@
 
-<div class="col-md-3 col-sm-6" style="background: #f2fcfc; margin: 10px 3px 5px 3px; padding: 10px; border-radius: 8px">
-    <div class="thumbnail no-border no-padding">
-        <div class="media">
-            <a class="media-link" data-gal="prettyPhoto" href="{{ url('public/store/assets/img/preview/shop/product-1-big.jpg') }}">
-                <?php $image_url = env('PRODUCTS_IMAGE_URL') . $product->main_image()?->image_url ?>
-                <img src='{{ $image_url ?? '' }}' alt=""/>
-                <span class="icon-view"><strong><i class="fa fa-eye"></i></strong></span>
-            </a>
-        </div>
-        <div class="caption text-center">
-            <h4 class="caption-title"><a href="{{ route('product-show', [ 'id' => $product->id ]) }}">{{ $product->name ?? '' }}</a></h4>
-            <div class="rating">
-                <span class="star"></span><!--
-                --><span class="star active"></span><!--
-                --><span class="star active"></span><!--
-                --><span class="star active"></span><!--
-                --><span class="star active"></span>
+<div class="col-md-3 col-sm-6">
+    <div style="margin: 10px 3px 8px 2px; background: #f2fcfc ; border-radius: 8px">
+        <div class="thumbnail no-border no-padding">
+            <div class="media" style="padding: 5px">
+                <a class="media-link" data-gal="prettyPhoto" href="{{ url('public/store/assets/img/preview/shop/product-1-big.jpg') }}">
+                    <?php $image_url = env('PRODUCTS_IMAGE_URL') . $product->main_image()?->image_url ?>
+                    <img src='{{ $image_url ?? '' }}' alt=""/>
+                    <span class="icon-view"><strong><i class="fa fa-eye"></i></strong></span>
+                </a>
             </div>
-            <div class="price"><ins>{{ $product?->min_price()->price ?? '' }} <span style="color: black">تومان</span></ins> <del>{{ $product->old_price ?? '' }}</del></div>
-            <div class="buttons">
-                <a class="btn btn-theme btn-theme-transparent btn-wish-list" href="#"><i class="fa fa-heart"></i></a><!--
-                --><a class="btn btn-theme btn-theme-transparent btn-icon-left" onclick="add_to_cart({{$product->min_price()?->product_producer_id}})"><i class="fa fa-shopping-cart"></i>افزودن به سبد</a><!--
-                --><a class="btn btn-theme btn-theme-transparent btn-compare" href="#"><i class="fa fa-exchange"></i></a>
+            <div class="caption text-center">
+                <h4 class="caption-title"><a href="{{ route('product-show', [ 'id' => $product->id ]) }}">{{ $product->name ?? '' }}</a></h4>
+                <div class="rating">
+                    <span class="star"></span><!--
+                    --><span class="star active"></span><!--
+                    --><span class="star active"></span><!--
+                    --><span class="star active"></span><!--
+                    --><span class="star active"></span>
+                </div>
+                <div class="price"><ins>{{ $product?->min_price()->price ?? '' }} <span style="color: black">تومان</span></ins> <del>{{ $product->old_price ?? '' }}</del></div>
+                <div class="buttons">
+                    <a class="btn btn-theme btn-theme-transparent btn-wish-list" href="#"><i class="fa fa-heart"></i></a><!--
+                    --><a class="btn btn-theme btn-theme-transparent btn-icon-left" onclick="add_to_cart({{$product->min_price()?->product_producer_id}})"><i class="fa fa-shopping-cart"></i>افزودن به سبد</a><!--
+                    --><a class="btn btn-theme btn-theme-transparent btn-compare" href="#"><i class="fa fa-exchange"></i></a>
+                </div>
             </div>
         </div>
     </div>
+    
 </div>
