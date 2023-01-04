576a975b57004131185363c870b1d47cf10f522b
mohammad shahab <mkhodroo@yahoo.com>
2023-01-04 20:06:24 +0330

    home swipper

--- resources/views/store/home/home.blade.php
+++ resources/views/store/home/home.blade.php
@@ -38,7 +38,7 @@
             <div class="swiper-wrapper">
               <!-- Slides -->
               <div class="text-center swiper-slide" >
-                <h2 class="width-200 white-color yekan-titr">جدیدترین ها</h2>
+                    <img class="width-200" src="{{ url('public/store/assets/img/new-products.png') }}" alt="new-products" style="width: 100%; position: relative; top: -50px">
               </div>
                 @foreach ($newest_products as $item)
                     <div class="swiper-slide">
@@ -56,48 +56,50 @@
             <div class="swiper-scrollbar"></div>
         </div>
 
-        <div class="row catagories table-responsive hide-scroll border-radius" id="catagories">
-            <table class="catagories-table" >
-                {{-- <div class="text-center scroll-btn" style="float: left; position: relative; top:47%; z-index: 200; height:0px">
-                    <button class="btn btn-default" onclick="scroll_left('catagories')" style="position: relative; top: 50%; border-radius: 50%" >></button>
-                </div> --}}
-                <tr>
-                    <td style="text-align: center">
-                        <h2 style="width: 200px; color: white" class=" yekan-titr">دسته بندی ها</h2>
-                    </td>
-                    <td class="text-center width-200">
-                        <a href="{{ route('show-catagory-by-part-of-name', ['name' => 'ایسیو های نسل دو']) }}">
-                            <img class="width-200" src="{{ url('public/store/assets/img/capsole.png') }}" alt="capsole" style="width: 100%">
-                            <h4 style="font-weight: bold" class="white-color">ایسیوهای نسل دو</h4>
-                        </a>
-                    </td>
-                    <td class="text-center width-200">
-                        <a href="{{ route('show-catagory-by-part-of-name', ['name' => 'مخزن']) }}">
-                            <img class="width-200" src="{{ url('public/store/assets/img/capsole.png') }}" alt="capsole" style="width: 100%">
-                            <h4 style="font-weight: bold" class="white-color">مخازن CNG</h4>
-                        </a>
-                    </td>
-                    <td class="text-center width-200">
-                        <a href="{{ route('show-catagory-by-part-of-name', ['name' => 'رگلاتور']) }}">
-                            <img class="width-200" src="{{ url('public/store/assets/img/cng-regolator.png') }}" alt="capsole" style="width: 100%">
-                            <h4 style="font-weight: bold" class="white-color">رگولاتورهای CNG</h4>
-                        </a>
-                    </td>
-                    <td class="text-center width-200">
-                        <a href="{{ route('show-catagory-by-part-of-name', ['name' => 'شیرمخزن']) }}">
-                            <img class="width-200" src="{{ url('public/store/assets/img/cng-valve.png') }}" alt="capsole" style="width: 100%">
-                            <h4 style="font-weight: bold" class="white-color">شیرمخزن CNG</h4>
-                        </a>
-                    </td>
-                    <td class="text-center width-200">
-                        <a href="{{ route('show-catagory-by-part-of-name', ['name' => 'شیرقطع کن']) }}">
-                            <img class="width-200" src="{{ url('public/store/assets/img/cng-cutoff-valve.png') }}" alt="capsole">
-                            <h4 style="font-weight: bold" class="white-color">شیرقطع کن های CNG</h4>
-                        </a>
-                    </td>
-                </tr>
-            </table>
+        <div class="swiper catagories">
             
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="text-center swiper-slide" >
+                    <h2 class="width-200 white-color yekan-titr">دسته بندی ها</h2>
+                </div>
+                <div class="swiper-slide text-center">
+                    <a href="{{ route('show-catagory-by-part-of-name', ['name' => 'ایسیو های نسل دو']) }}">
+                        <img class="width-200" src="{{ url('public/store/assets/img/ecu.png') }}" alt="capsole" style="width: 100%">
+                        <h4 style="font-weight: bold" class="white-color">ایسیوهای نسل دو</h4>
+                    </a>
+                </div>
+                <div class="swiper-slide text-center">
+                    <a href="{{ route('show-catagory-by-part-of-name', ['name' => 'مخزن']) }}">
+                        <img class="width-200" src="{{ url('public/store/assets/img/capsole.png') }}" alt="capsole" style="width: 100%">
+                        <h4 style="font-weight: bold" class="white-color">مخازن CNG</h4>
+                    </a>
+                </div>
+                <div class="swiper-slide text-center">
+                    <a href="{{ route('show-catagory-by-part-of-name', ['name' => 'رگلاتور']) }}">
+                        <img class="width-200" src="{{ url('public/store/assets/img/cng-regolator.png') }}" alt="capsole" style="width: 100%">
+                        <h4 style="font-weight: bold" class="white-color">رگولاتورهای CNG</h4>
+                    </a>
+                </div>
+                <div class="swiper-slide text-center">
+                    <a href="{{ route('show-catagory-by-part-of-name', ['name' => 'شیرمخزن']) }}">
+                        <img class="width-200" src="{{ url('public/store/assets/img/cng-valve.png') }}" alt="capsole" style="width: 100%">
+                        <h4 style="font-weight: bold" class="white-color">شیرمخزن CNG</h4>
+                    </a>
+                </div>
+                <div class="swiper-slide text-center">
+                    <a href="{{ route('show-catagory-by-part-of-name', ['name' => 'شیرقطع کن']) }}">
+                        <img class="width-200" src="{{ url('public/store/assets/img/cng-cutoff-valve.png') }}" alt="capsole">
+                        <h4 style="font-weight: bold" class="white-color">شیرقطع کن های CNG</h4>
+                    </a>
+                </div>
+            </div>
+          
+            <!-- If we need scrollbar -->
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-scrollbar"></div>
         </div>
 
         
@@ -108,11 +110,14 @@
             if(w <= 600){
                 spv = 1.5;
             }
-            if( w >= 600 && w <= 999){
-                spv = 2
+            if( w >= 600 && w <= 1050){
+                spv = 2.5
             }
-            if( w >= 999){
-                spv = 5
+            if(w >= 1050 && w <= 1200){
+                spv = 3.5
+            }
+            if(w >= 1200){
+                spv = 4.5;
             }
             console.log(spv);
             var swiper = new Swiper(".swiper", {
